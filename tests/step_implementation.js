2f968e1424be2a05c6756c83a15e5d56c58ddc31
UshaSai-154 <69130942+UshaSai-154@users.noreply.github.com>
2020-08-06 10:50:12 +0530

    Update step_implementation.js
    Temporary changes

--- tests/step_implementation.js
+++ tests/step_implementation.js
@@ -37,11 +37,11 @@ step("Click on Clinical Service", async () => {
   
  step("Enter Username", async () => {
     console.log("USERNAME : "+process.env.USERNAME);
-    await write(process.env.USERNAME,into(textBox({placeholder: locators.Details.Username})));
+    await write(data.Registration.username,into(textBox({placeholder: locators.Details.Username})));
  });
   
  step("Enter Password", async () => {
-    await write(process.env.PASSWORD,into(textBox({placeholder: locators.Details.Password})));
+    await write(data.Registration.password,into(textBox({placeholder: locators.Details.Password})));
  });
   
  step("Select Location", async () => {
